7f286ba2ff291f22fddfd503949ec44a5681b3cd
alexwander <ryndyuk.alexander@gmail.com>
2016-08-14 22:34:48 +0300

    changes in js

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ var app = express();
 var port = process.env.PORT || 5001;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', 'src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.send('hello no');
+    res.render('index', {
+        title: 'Hello from render',
+        nav: [{
+            Link: '/Books',
+            Text: 'Books'
+        }, {
+            Link: '/Authors',
+            Text: 'Authors'
+        }]
+    });
+});
 
+app.get('/books', function (req, res) {
+    res.send('Hello Books');
 });
 
 app.listen(port, function (err) {
