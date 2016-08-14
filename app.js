9690fa22649d39bfd01d87b36a37f55e6c533b08
alexwander <ryndyuk.alexander@gmail.com>
2016-08-14 15:36:54 +0300

    add some config

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var app = express();
 
-var port = 5001;
-app.listen(port, function(err){
-    console.log('running server on port ' + port);
+var port = process.env.PORT || 5001;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function (req, res) {
+    res.send('hello no');
+
 });
+
+app.listen(port, function (err) {
+    console.log('ALL OK running server on port ' + port);
+});
+
