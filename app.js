54eede1f824ef04defe8f3e6ee9d52c6b9100f5e
alexwander <ryndyuk.alexander@gmail.com>
2016-08-23 16:45:44 +0300

    new ver of js files

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 var express = require('express');
+
 var app = express();
 
-var port = process.env.PORT || 5001;
+var port = process.env.PORT || 5000;
+var nav = [{
+    Link: '/Books',
+    Text: 'Book'
+    }, {
+    Link: '/Authors',
+    Text: 'Author'
+    }];
+var bookRouter = require('./src/routes/bookRoutes')(nav);
+var adminRouter = require('./src/routes/adminRoutes')(nav);
+
 
 app.use(express.static('public'));
-app.set('views', 'src/views');
+app.set('views', './src/views');
+
 app.set('view engine', 'ejs');
+
+
+app.use('/Books', bookRouter);
+app.use('/Admin', adminRouter);
 
 app.get('/', function (req, res) {
     res.render('index', {
@@ -27,4 +43,3 @@ app.get('/books', function (req, res) {
 app.listen(port, function (err) {
     console.log('ALL OK running server on port ' + port);
 });
-
--- gulpfile.js
+++ gulpfile.js
@@ -31,75 +31,27 @@ gulp.task('inject', function () {
         bowerJson: require('./bower.json'),
         directory: './public/lib',
         ignorePath: '../../public'
-    };
+    }
 
     return gulp.src('./src/views/*.jade')
         .pipe(wiredep(options))
         .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
 
-});
-
-// gulp.task('serve', ['inject'], function () {
-//     var options = {
-//         script: 'app.js',
-//         delayTime: 1,
-//         env: {
-//             'PORT': 3001
-//         },
-//         watch: jsFiles
-//     };
-
-//     return nodemon(options)
-//         .on('restart', function (ev) {
-//             console.log('Restarting....');
-//         });
-// });
-
-
-
-// gulp.task('serve', function () {
-//   nodemon ({
-//     'script': 'app.js',
-//     'ext': 'js html',
-//     'ignore': [
-//         '.git',
-//         'node_modules',
-//         'test'],
-
-//     env: { 'PORT': 3001 },
-//     delayTime: 1,
-
-
-//   });
-// return nodemon(options)
-//         .on('restart', function (ev) {
-//             console.log('Restarting....');
-//         });
-
-
-
-// });
-
-
-var notify = require('gulp-notify');
-var livereload = require('gulp-livereload');
- 
-// Task
-gulp.task('default', function() {
-	// listen for changes
-	livereload.listen();
-	// configure nodemon
-	nodemon({
-		// the script to run the app
-		script: 'app.js',
-		ext: 'js'
-	}).on('restart', function(){
-		// when the app has restarted, run livereload.
-		gulp.src('app.js')
-			.pipe(livereload())
-	})
 })
 
+gulp.task('serve', ['inject'], function () {
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 5000
+        },
+        watch: jsFiles
+    }
 
-
+    return nodemon(options)
+        .on('restart', function (ev) {
+            console.log('Restarting....');
+        })
+})
