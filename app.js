4214fc3459f15ad4f75d49e49d1dd0c5d66afa3b
alexwander <ryndyuk.alexander@gmail.com>
2016-08-28 21:55:04 +0300

    add new login routes

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
 
 var app = express();
 
@@ -6,22 +10,29 @@ var port = process.env.PORT || 5000;
 var nav = [{
     Link: '/Books',
     Text: 'Book'
-    }, {
+}, {
     Link: '/Authors',
     Text: 'Author'
-    }];
+}];
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
-
+var authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(session({secret: 'library'}));
+
+require('./src/config/passport')(app);
+
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
 
-
 app.use('/Books', bookRouter);
 app.use('/Admin', adminRouter);
+app.use('/Auth', authRouter);
 
 app.get('/', function (req, res) {
     res.render('index', {
@@ -41,5 +52,5 @@ app.get('/books', function (req, res) {
 });
 
 app.listen(port, function (err) {
-    console.log('ALL OK running server on port ' + port);
+    console.log('all ok running server on port ' + port);
 });
